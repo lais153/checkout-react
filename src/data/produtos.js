b4d03f0d9a175303880754a912803fb235809d56
lais153 <leticialaissm@gmail.com>
2026-09-14 22:51:10 -0300

    data: criacao do set de dados estaticos para listagem da sacola

---
+++ src/data/produtos.js
@@ -0,0 +1,6 @@
+export const LISTA_PRODUTOS = [
+  { id: 201, nome: "Bolsa de Couro Legítimo Classic", precoUnitario: 389.90, quantidade: 1 },
+  { id: 202, nome: "Perfume Eau de Parfum Intense 50ml", precoUnitario: 299.00, quantidade: 1 },
+  { id: 203, nome: "Batom Matte Longa Duração Hydra", precoUnitario: 85.50, quantidade: 2 }
+];
+ 
